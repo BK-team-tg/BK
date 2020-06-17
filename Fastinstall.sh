f91f5623cfee727b2c54c9649763048fceb1ddf6
BK-team-tg <67076331+BK-team-tg@users.noreply.github.com>
2020-06-18 00:10:54 +0300

    Update Fastinstall.sh

--- Fastinstall.sh
+++ Fastinstall.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-cd $HOME/tumblr-Source
+cd $HOME/BK
 rm -rf $HOME/.telegram-cli
 install() {
 rm -rf $HOME/.telegram-cli
 sudo chmod +x tg
-chmod +x tumblr-Source
+chmod +x BK
 chmod +x ts
 ./ts
 }
